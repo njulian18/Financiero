ddc14920b8f1bbd09731a71bec2f2ed7e2ec0e8f
Julian Nuñez <69222010+njulian18@users.noreply.github.com>
2022-11-02 22:26:48 -0300

    frances

---
+++ js/amortizacion.js
@@ -0,0 +1,54 @@
+const monto = document.getElementById("monto");
+const tiempo = document.getElementById("tiempo");
+const interes = document.getElementById("interes");
+const btnCalcular = document.getElementById("btnCalcular");
+const llenarTabla = document.querySelector("#lista-tabla tbody");
+const usuario = "usuario";
+const operador = "operador";
+
+btnCalcular.addEventListener("click", () => {
+  calcularCuota(monto.value, interes.value, tiempo.value);
+});
+
+function calcularCuota(monto, interes, tiempo) {
+  while (llenarTabla.firstChild) {
+    llenarTabla.removeChild(llenarTabla.firstChild);
+  }
+  count = 0;
+  let fechas = [];
+  let fechaActual = Date.now();
+  let mes_actual = moment(fechaActual);
+  mes_actual.add(1, "month");
+
+  let pagoInteres = 0,
+    pagoCapital = 0,
+    cuota = 0;
+
+  cuota =
+    (monto * ((Math.pow(1 + interes / 100, tiempo) * interes) / 100)) /
+    (Math.pow(1 + interes / 100, tiempo) - 1);
+
+  for (let i = 1; i <= tiempo; i++) {
+    count++;
+    pagoInteres = parseFloat(monto * (interes / 100));
+    pagoCapital = cuota - pagoInteres;
+    monto = parseFloat(monto - pagoCapital);
+
+    //Formato fechas
+    fechas[i] = mes_actual.format("DD-MM-YYYY");
+    mes_actual.add(1, "month");
+
+    const row = document.createElement("tr");
+    row.innerHTML = `
+            <td>${fechas[i]}</td>
+            <td>${cuota.toFixed(2)}</td>
+            <td>${count+'/'+tiempo}</td>
+            <td>${pagoCapital.toFixed(2)}</td>
+            <td>${pagoInteres.toFixed(2)}</td>
+            <td>${monto.toFixed(2)}</td>
+            <td>${usuario}</td>
+            <td>${operador}</td>
+        `;
+    llenarTabla.appendChild(row);
+  }
+}
